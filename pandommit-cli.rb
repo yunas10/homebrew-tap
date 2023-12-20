4e2e602eba36c5cdf18aca5fef5f88631995d3b6
yunas10 <108397723+yunas10@users.noreply.github.com>
2023-12-20 22:01:43 +0100

    🌟feat: tag change

--- pandommit-cli.rb
+++ pandommit-cli.rb
@@ -1,8 +1,8 @@
 class PandommitCli < Formula
   desc "Pandommit-cli is a command-line tool designed to create Git commits using standardized naming conventions. It aims to structure commit messages in a consistent and intuitive way."
   homepage "https://github.com/yunas10/pandommit-cli"
-  url "https://github.com/yunas10/pandommit-cli/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "423c4139c09343aa8fddb332c23dc2fa3ef5dfaf7da764ec395d48761a6d8f4f"
+  url "https://github.com/yunas10/pandommit-cli/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "e5b8a7ffb265d2b64786ef6d291f038d389671ade586cd38f9d213b39797c2e1"
   version "1.0.2"
   license "Apache-2.0"
 
